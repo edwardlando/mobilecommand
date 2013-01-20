aad1786241b8ffe909251aa99a3e6747a0f0a443
Patrick Wingo <pbwingo@gmail.com>
2013-01-19 21:32:36 -0500

    added business

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,9 @@
 module PostsHelper
 
 	def nyt(second)
+		base_uri = ""
 		if (second == "TOP")
 			base_uri = "http://api.nytimes.com/svc/mostpopular/v2/mostshared/all-sections/1.json?api-key=39186a552e64bb003eb882b3a7486aba:10:67206205"
-
 			http = Curl.get(base_uri)
 
 			json_body = JSON.parse(http.body_str)
@@ -25,6 +25,31 @@ module PostsHelper
 			o =  [('a'..'z')].map{|i| i.to_a}.flatten
 			shortcode  =  (0...3).map{ o[rand(o.length)] }.join
 			textback += "\nmblmstr://nyt/top"
+			puts textback
+			return textback
+		elsif (second = "BUS")
+			base_uri = "http://api.nytimes.com/svc/mostpopular/v2/mostshared/business/1.json?api-key=39186a552e64bb003eb882b3a7486aba:10:67206205"
+			http = Curl.get(base_uri)
+
+			json_body = JSON.parse(http.body_str)
+			results = json_body['results']
+			top_titles = []
+			results.each.with_index do |res, ind|
+				if (ind < 6)
+					top_titles.push(res['title'])
+				end
+			end
+
+			puts top_titles
+			textback = "Top stories today:\n" 
+			top_titles.each.with_index do |title,ind|
+				textback+=""+ind+") " + title + "\n"
+			end
+#			textback+=+ "1) " + top_titles[0]+"\n" +  "2) " + top_titles[1]+"\n" +"3) " + top_titles[2]
+
+			o =  [('a'..'z')].map{|i| i.to_a}.flatten
+			shortcode  =  (0...3).map{ o[rand(o.length)] }.join
+			textback += "\nmblmstr://nyt/bus"
 			puts textback
 			return textback
 		elsif (second == "1")
