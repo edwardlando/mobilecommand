7fdee10a683169c0029c964132802638fc0dd463
Patrick Wingo <pbwingo@gmail.com>
2013-01-19 20:47:34 -0500

    added abstract request for nyt

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,6 @@ module PostsHelper
 
 	def nyt(second)
 		if (second == "TOP")
-			puts "in second part top"
 			base_uri = "http://api.nytimes.com/svc/mostpopular/v2"
 			base_uri+="/mostshared/all-sections/1.json"
 			base_uri += "?api-key=39186a552e64bb003eb882b3a7486aba:10:67206205"
@@ -20,10 +19,37 @@ module PostsHelper
 			end
 
 			puts top_titles
-			textback = top_titles[0]+" " +top_titles[1]+" " +top_titles[2]
+			textback = "Top stories today:\n" + "1) " + top_titles[0]+"\n" +  "2)" + top_titles[1]+"\n" +"3)" + top_titles[2]
+
+			o =  [('a'..'z')].map{|i| i.to_a}.flatten
+			shortcode  =  (0...3).map{ o[rand(o.length)] }.join
+			textback += ("\nNYT/"+shortcode)
 			puts textback
 			return textback
+		elsif (second == "1")
+			article_request(1)
+		elsif (second == "2")
+			article_request(2)
+		elsif (second == "3")
+			article_request(3)
 		end
+	end
+
+	def article_request(num)
+			base_uri = "http://api.nytimes.com/svc/mostpopular/v2"
+			base_uri+="/mostshared/all-sections/1.json"
+			base_uri += "?api-key=39186a552e64bb003eb882b3a7486aba:10:67206205"
+
+			http = Curl.get(base_uri)
+			json_body = JSON.parse(http.body_str)
+			results = json_body['results']
+			top_titles = []
+			results.each.with_index do |res, ind|
+				if ((ind+1) == num)
+					return res['abstract']
+				end
+			end
+			return "Please request one of the three chosen articles"
 	end
 
 end
