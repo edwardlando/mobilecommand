dfa7040f65b8b96d44335abdf63dc4aacaa229c9
edwardlando <edwardlando8@gmail.com>
2014-03-03 17:39:02 -0500

    testing

---
+++ NoticeWebsite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoticeWebsite::Application.config.secret_token = '603184fb9e0647f030bdec69be3ea1a61a0c6471f0ea7bbf72b7de59c459f04ed5339f7e0d6e32918137916e584171aff7833692f6bead8f96259247df5daf83'
